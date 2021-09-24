e06cdf2db2a99339c65baa55002e3aaca9a758e9
Ercio Alendre <ercio.alendre@gmail.com>
2021-09-24 15:31:28 -0300

    service to create contacts was created

---
+++ src/modules/contacts/services/CreateContact.service.ts
@@ -0,0 +1,35 @@
+import { Response } from "express";
+import UsersRepository from "@modules/users/models/repositories/Users.repository";
+import ContactsRepository from "../models/repositories/Contacts.repository";
+import { IContact } from "../models/Contacts.model";
+import ListContactsService from "./ListContacts.service";
+
+export default class CreateContactService {
+  static async execute(
+    { contact_owner_id, contact_name, contact_phone, contact_email = "" }: IContact,
+    res: Response,
+  ): Promise<void | boolean> {
+    const contactOwnerExists = await UsersRepository.findById(contact_owner_id);
+
+    if (contactOwnerExists) {
+      const createContact = await ContactsRepository.create({
+        contact_owner_id,
+        contact_name,
+        contact_phone,
+        contact_email,
+      });
+
+      if (createContact) {
+        const contacts = await ListContactsService.execute(res);
+
+        res.render("main", {
+          page: "my-account",
+          msgContent: "Contato criado com sucesso",
+          inputError: "",
+          msgType: "success",
+          contacts,
+        });
+      }
+    }
+  }
+}
