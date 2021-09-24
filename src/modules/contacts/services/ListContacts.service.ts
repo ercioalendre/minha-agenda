2f4b99b125acafbf6226cdda17f0a642e1afbd6d
Ercio Alendre <ercio.alendre@gmail.com>
2021-09-24 15:32:21 -0300

    service to list contacts was created

---
+++ src/modules/contacts/services/ListContacts.service.ts
@@ -0,0 +1,26 @@
+import { Response } from "express";
+import UsersRepository from "@modules/users/models/repositories/Users.repository";
+import ContactsRepository from "@modules/contacts/models/repositories/Contacts.repository";
+import mongoose, { ObjectId } from "mongoose";
+
+export default class ListContactsService {
+  static async execute(
+    res: Response,
+  ): Promise<(mongoose.Document & { _id: ObjectId })[] | null | undefined> {
+    const userId = res.locals.user.id;
+    const userExists = await UsersRepository.findById(userId);
+
+    if (userExists) {
+      const contacts = await ContactsRepository.getAllContacts(userId);
+      return contacts;
+      // return res.render("main", {
+      //   page: "my-account",
+      //   msgType: "",
+      //   msgContent: "",
+      //   inputError: "",
+      //   formData: {},
+      //   contacts,
+      // });
+    }
+  }
+}
