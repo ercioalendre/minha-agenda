ee44bd925a9c4328141024b6bdd17091e72ad94f
Ercio Alendre <ercio.alendre@gmail.com>
2021-09-27 11:21:55 -0300

    added edit method to contacts controller

--- src/modules/contacts/controllers/Contacts.controller.ts
+++ src/modules/contacts/controllers/Contacts.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import AppError from "@shared/errors/AppError";
 import CreateContactService from "../services/CreateContact.service";
 import DeleteContactService from "../services/DeleteContact.service";
+import EditContactService from "../services/EditContact.service";
 
 export default class ContactsController {
   static async create(req: Request, res: Response): Promise<void> {
@@ -21,10 +22,28 @@ export default class ContactsController {
     }
   }
 
-  static async delete(req: Request, res: Response): Promise<void> {
-    const { contactId } = req.body;
+  static async edit(req: Request, res: Response): Promise<void> {
+    const { editContactId, editContactName, editContactPhone, editContactEmail } = req.body;
     try {
-      await DeleteContactService.execute(res.locals.user.id, contactId, res);
+      await EditContactService.execute(
+        {
+          contact_owner_id: res.locals.user.id,
+          contact_id: editContactId,
+          contact_name: editContactName,
+          contact_phone: editContactPhone,
+          contact_email: editContactEmail,
+        },
+        res,
+      );
+    } catch (error) {
+      throw new AppError(error as string);
+    }
+  }
+
+  static async delete(req: Request, res: Response): Promise<void> {
+    const { deleteContactId } = req.body;
+    try {
+      await DeleteContactService.execute(res.locals.user.id, deleteContactId, res);
     } catch (error) {
       throw new AppError(error as string);
     }
